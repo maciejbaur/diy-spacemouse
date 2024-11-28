d40155ff7bba77fbcdcf99f12b9610e43e649d8d
Maciej Baur <maciej.baur@gmail.com>
2024-11-28 15:12:45 +0100

    Update diy-spacemouse.ino
    Zmiany sugerowane przez burke_dev

--- Code/diy-spacemouse/diy-spacemouse.ino
+++ Code/diy-spacemouse/diy-spacemouse.ino
@@ -12,6 +12,7 @@ OneButton button2(24, true);
 
 float xOffset = 0, yOffset = 0, zOffset = 0;
 float xCurrent = 0, yCurrent = 0, zCurrent = 0;
+unsigned long lastMagUpdate;
 
 int calSamples = 300;     // (300)
 int sensivity = 9;        // (8) czulosc
@@ -19,9 +20,9 @@ int magRange = 3;         // (3)
 int outRange = 127;       // (127)
 float xyThreshold = 0.5;  // wartosc powyzej ktorej przesuwa sie kursor
 float zThreshold = 1.8;   // wartosc powyzej ktorej jest zmiana z obracania na przesuwanie modelu
-float xCorr = -0.60;      // korekta w pionie
-float yCorr = -0.60;      // korekta w poziomie
-float zCorr =  0.00;      // korekta nacisku
+// float xCorr = -0.60;      // korekta w pionie
+// float yCorr = -0.60;      // korekta w poziomie
+// float zCorr =  0.00;      // korekta nacisku
 
 int inRange = magRange * sensivity;
 // float lastMagUpdate;
@@ -42,22 +43,24 @@ void setup() {
   Serial.begin(9600);
   Wire1.begin();
 
+  // Magnetometer Sensor Init
   mag.begin(Wire1);
   mag.setAccessMode(mag.MASTERCONTROLLEDMODE);
-  //mag.setCheckFrameCountError(true);
+  mag.setCheckFrameCountError(true); // new line from @burke_dev
   mag.disableTemp();
-  //calibrateSensor();
+  calibrateSensor();
 
-  for (int i = 1; i <= calSamples; i++) {
-    delay(mag.getMeasurementDelay());
-    mag.updateData();
+  // Kalibracja sensora przeniesiona do funkcji calibrateSensor
+  // for (int i = 1; i <= calSamples; i++) {
+  //   delay(mag.getMeasurementDelay());
+  //   mag.updateData();
 
-    xOffset += mag.getX();
-    yOffset += mag.getY();
-    zOffset += mag.getZ();
+  //   xOffset += mag.getX();
+  //   yOffset += mag.getY();
+  //   zOffset += mag.getZ();
 
-    Serial.print(".");
-  }
+  //   Serial.print(".");
+  // }
 
   xOffset = xOffset / calSamples;
   yOffset = yOffset / calSamples;
@@ -74,20 +77,24 @@ void loop() {
   button2.tick();
 
   delay(mag.getMeasurementDelay());
-  mag.updateData();
+  // mag.updateData(); // replaced by new code from @burke_dev
 
   // Update the magnetometer
-  // Tlv493d_Error updateDataResult = mag.updateData();
-  // lastMagUpdate = millis();
+  Tlv493d_Error updateDataResult = mag.updateData();
+  lastMagUpdate = millis();
 
   // Value of 2 means ADC hang-up has occurred. Reset the sensor to fix.
-  // if (updateDataResult == 2) {
-  //   return resetSensor();
-  // }
+  if (updateDataResult == 2) {
+    return resetSensor();
+  }
 
-  xCurrent = yFilter.updateEstimate((mag.getY() - xOffset)) + xCorr;     // ruch w pionie z korekta
-  yCurrent = xFilter.updateEstimate((mag.getX() - yOffset)*-1) + yCorr;  // ruch w poziomie z korekta
-  zCurrent = zFilter.updateEstimate((mag.getZ() - zOffset)) + zCorr;     // nacisk z korekta
+  // xCurrent = yFilter.updateEstimate((mag.getY() - xOffset)) + xCorr;     // ruch w pionie z korekta
+  // yCurrent = xFilter.updateEstimate((mag.getX() - yOffset)*-1) + yCorr;  // ruch w poziomie z korekta
+  // zCurrent = zFilter.updateEstimate((mag.getZ() - zOffset)) + zCorr;     // nacisk z korekta
+
+  xCurrent = yFilter.updateEstimate((mag.getY() - xOffset));     // ruch w pionie
+  yCurrent = xFilter.updateEstimate((mag.getX() - yOffset)*-1);  // ruch w poziomie
+  zCurrent = zFilter.updateEstimate((mag.getZ() - zOffset));     // nacisk
 
   if (abs(xCurrent) > xyThreshold || abs(yCurrent) > xyThreshold) {     // jesli wartosci przesuniecia x i y sa powyzej x i y Threshold to ruszaj kursorem
     int xMove = map(xCurrent, -inRange, inRange, -outRange, outRange);
@@ -138,11 +145,24 @@ void fitToScreen() {
   Serial.println("pressed fit");
 }
 
-// void resetSensor() {
-//   mag.end();
-//   mag.begin(Wire1);
-//   mag.setAccessMode(mag.MASTERCONTROLLEDMODE);
-//   mag.setCheckFrameCountError(true);
-//   mag.disableTemp();
-//   delay(100);
-// }
+void calibrateSensor() {
+  for (int i = 1; i <= calSamples; i++) {
+    delay(mag.getMeasurementDelay());
+    mag.updateData();
+
+    xOffset += mag.getX();
+    yOffset += mag.getY();
+    zOffset += mag.getZ();
+
+    Serial.print(".");
+  }
+}
+
+void resetSensor() {
+  mag.end();
+  mag.begin(Wire1);
+  mag.setAccessMode(mag.MASTERCONTROLLEDMODE);
+  mag.setCheckFrameCountError(true);
+  mag.disableTemp();
+  delay(100);
+}
